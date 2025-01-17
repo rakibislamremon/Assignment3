73d4d5f4f79e7641f6bd1b7004c7c729a9bf8d59
rakibulislamremon <mdrakibulislamremon@gmail.com>
2025-01-17 20:02:06 +0600

    Added app

--- routes/api.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-// import { login } from "../app/controllers/todoController.js";
-import * as TodoController from "../app/controllers/todoController.js"
-const router = express.Router();
-
-router.get("/login", TodoController.login);
-
-export default router;
